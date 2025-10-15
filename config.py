87d4857c02543239c02e7dcc79ff4b3e703ce0ff
nrecover4100-wq <nrecover4100@gmail.com>
2025-10-15 09:15:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("11217417"))
+API_HASH = getenv("1242ca480c1526dc96e710021f7f6e85")
 
-STRING_SESSION = getenv("STRING_SESSION")
-BOT_LIST = getenv("BOT_LIST", "").split()
+STRING_SESSION = getenv("AQG4HCQAPpBLNbm6hqrIFIsG_pHs6EtWVbJC7bSi521JHttlE0JlgfYQwKDLj3N0YHCyhrWjb72wH6h2EEe7qXa-qh57xXhxAJMYq-NcoHji22Vew0MfQmnBxMMcuBAsROfUvbCJfcABiegobfaYa7BQ7YepYGeWLHmR3DfMKJpdkV8JqdN1BJeJN2TVp9zQx2P-bg01535_0UggpA3HvBGipbTVozrV0tjZS0zRUFBFuP3febrzxtmBCGL5KGBe4r0lEmWU8ond4GbR5zfXLfgRpwlSwJF9HWly5EOMKkwCxqc55rHWxf_IuEKm7IkGiS7MiIiSc6I-7ow4fPIV8KbH0iEfHAAAAAHRiCQ-AA")
+BOT_LIST = getenv("7730811812:AAG-o2OGtsAQxIuNaOfS25Eo_d4mCORi_g8, "").split()
 
-CHANNEL_ID = int(getenv("CHANNEL_ID"))
-MESSAGE_ID = int(getenv("MESSAGE_ID"))
+CHANNEL_ID = int(getenv("-4906128756"))
+MESSAGE_ID = int(getenv("-4906128756"))
